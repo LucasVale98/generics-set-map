ffb62da4997ca884caa116b4eb18454412f87ff4
Lucas Vale <lucasvale1998.lv@gmail.com>
2020-01-09 18:14:09 -0300

    Introducao aos Generics

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,28 @@
 package application;
 
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.print.PrintService;
+
 public class Program {
 	public static void main(String[] args) {
-		System.out.println("generics-set-map");
+		Scanner sc = new Scanner(System.in);
+
+		PrintService ps = new PrintService();
+		
+		System.out.print("How many values? ");
+		int n = sc.nextInt();
+		
+		for (int i = 0; i < n; i++) {
+			int value = sc.nextInt();
+			ps.addValue(value);
+		}
+
+		ps.print();
+		System.out.println("First: " + ps.first());
+		
+		sc.close();
 	}
 }
